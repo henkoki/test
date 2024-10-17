cfa3da736a497963fce8a20f14c8ee94e90ff04a
henkoki <127409040+henkoki@users.noreply.github.com>
2024-10-17 15:59:29 +0200

    Add files via upload

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -55,21 +55,9 @@ const initAutocomplete = () => {
     const place = autocomplete.getPlace();
     if (!place.geometry) return;
 
-    const gymKeywords = ['gym', 'fitness', 'health club', 'workout', 'training'];
-    const placeName = place.name.toLowerCase();
-    const isGymRelated = gymKeywords.some(keyword => 
-      placeName.includes(keyword.toLowerCase())
-    );
-
-    if (isGymRelated) {
-      setGymName(place.name);
-      setGymAddress(place.formatted_address);
-      setError(''); // Clear any previous error
-    } else {
-      setError(t('Please select a gym or fitness-related place'));
-      setGymName('');
-      setGymAddress('');
-    }
+    setGymName(place.name);
+    setGymAddress(place.formatted_address);
+    setError(''); // Clear any previous error
   });
 
   setAutocomplete(autocomplete);
